e362d91bf0d40208108be405ffa8eab1b0d4d3b9
Daniel Soltys <danielsytlos@gmail.com>
2024-09-01 15:31:15 +0200

    Cislovanie plochy zmenene od 0 a pridana funkcionalita viacerych slov v jednom riadku alebo stlpci

--- krizovka.pl
+++ krizovka.pl
@@ -9,12 +9,13 @@ krizovka(Slovnik, Tajanka, TajankaSmer, VyskaKrizovky, SirkaKrizovky) :-
 
 najdi_krizovku(Slovnik, Tajanka, TajankaSmer, VyskaKrizovky, SirkaKrizovky, PolozeneSlova, Grid) :-
     % Vytvorime si plochu krizovky
+    % Okienka krizovky cislujeme od 0 do (VyskaKrizovky * SirkaKrizovky - 1)
     vytvor_plochu(VyskaKrizovky, SirkaKrizovky, VytvorenaPlocha),
 
-    % Dostanem cislo Okienka, kde chceme aby zacinala nasa Tajanka
+    % Dostanem cislo Okienka, kde chceme aby zacinala nasa tajanka
     vrat_okienko_tajanky(VyskaKrizovky, SirkaKrizovky, TajankaSmer, OkienkoTajanky), % OkienkoTajanky je okienko, kde zacina tajanka
     
-    % Vlozime Tajanku do krizovky
+    % Vlozime tajanku do krizovky
     prirad_tajanku(Tajanka, TajankaSmer, OkienkoTajanky, VyskaKrizovky, SirkaKrizovky, VytvorenaPlocha, PlochaSTajankou),
 
     % Vygeneruje pozicie, kde budeme chciet umiestnit nase slova okrem Tajanky
@@ -36,8 +37,17 @@ prirad_slova(Slova, PolozeneSlova, [Pozicia|ZvysnePozicie], VyskaKrizovky, Sirka
     length(ListPismen, DlzkaSlova),
     Pozicia = Okienko-Smer,
     prirad_slovo(Slovo, ListPismen, DlzkaSlova, Napoveda, Okienko, Smer, VyskaKrizovky, SirkaKrizovky, VstupnyGrid, VytvorenaPlocha, PolozeneSlovo),
+    
+    % Ak slovo nedosahuje az na okraj plochy krizovky, tak pridaj medzi pozicie nove 2 pozicie od konca slova
+    ( vrat_okienko_za_koncom(DlzkaSlova, Okienko, Smer, VyskaKrizovky, SirkaKrizovky, OkienkoZaKoncom) ->
+        NovaPozicia = OkienkoZaKoncom-Smer,
+        ZvysnePoziciePridane = [NovaPozicia|ZvysnePozicie]
+    ;
+        ZvysnePoziciePridane = ZvysnePozicie 
+    ),
+
     vymaz([Slovo, Napoveda], Slova, PremazaneSlova),
-    prirad_slova(PremazaneSlova, [PolozeneSlovo|PolozeneSlova], ZvysnePozicie, VyskaKrizovky, SirkaKrizovky, VytvorenaPlocha, VystupnyGrid, PolozeneSlovaOut).
+    prirad_slova(PremazaneSlova, [PolozeneSlovo|PolozeneSlova], ZvysnePoziciePridane, VyskaKrizovky, SirkaKrizovky, VytvorenaPlocha, VystupnyGrid, PolozeneSlovaOut).
 
 % ----------------------------------------------------------------
 
@@ -50,12 +60,12 @@ prirad_pismena([], _, _, [], G, G).
 prirad_pismena([Pismeno|ZvysokPismen], Smer, Dlzka, [Okienko|ZvysokOkienok], VstupnyGrid, VystupnyGrid):-
     get_assoc(Okienko, VstupnyGrid, X),
     (
-    % AK tam je rovnake pismeno tak parada
+    % AK tam je rovnake pismeno, tak parada
     X == Pismeno,
     VytvorenaPlocha = VstupnyGrid
     ;
     % Ak tam je prazdne, tak to tam pridam
-    X == empty,
+    X == prazdne,
     put_assoc(Okienko, VstupnyGrid, Pismeno, VytvorenaPlocha)
     ), !, 
     prirad_pismena(ZvysokPismen, Smer, Dlzka, ZvysokOkienok, VytvorenaPlocha, VystupnyGrid).
@@ -66,16 +76,10 @@ prirad_pismena([Pismeno|ZvysokPismen], Smer, Dlzka, [Okienko|ZvysokOkienok], Vst
 % ----------------------------------------------------------------
 % Predikat, ktory podla Zaciatocneho okienka a Smeru priradi Slovo na plochu krizovky
 prirad_slovo(Slovo, ListPismen, DlzkaSlova, Napoveda, OkienkoZaciatku, Smer, VyskaKrizovky, SirkaKrizovky, VstupnyGrid, VystupnyGrid, PolozeneSlovo) :-
-    get_cisla_okienok_pre_slovo(DlzkaSlova, OkienkoZaciatku, Smer, Okienka, VyskaKrizovky, SirkaKrizovky),
+    vrat_cisla_okienok_pre_slovo(DlzkaSlova, OkienkoZaciatku, Smer, Okienka, VyskaKrizovky, SirkaKrizovky),
     prirad_pismena(ListPismen, Smer, DlzkaSlova, Okienka, VstupnyGrid, VystupnyGrid),
-    Okienka = [PrveOkienko|_],
-    ( Smer = doprava ->
-        NewPrveOkienkoZeroBased is PrveOkienko // SirkaKrizovky,  % Divide PrveOkienko by SirkaKrizovky if Smer is 'dole'
-        NewPrveOkienko is NewPrveOkienkoZeroBased + 1
-    ; 
-        NewPrveOkienko = PrveOkienko  % Otherwise, keep PrveOkienko as is
-    ),
-    PolozeneSlovo = [Smer, NewPrveOkienko, Napoveda].  % Use the new or unchanged value of PrveOkienko
+    Okienka = [ZaciatocneOkienko|_],
+    PolozeneSlovo = [Smer, ZaciatocneOkienko, Napoveda].
 % ----------------------------------------------------------------
 
 
@@ -86,13 +90,13 @@ prirad_slovo(Slovo, ListPismen, DlzkaSlova, Napoveda, OkienkoZaciatku, Smer, Vys
 generate_first_row(0, _, _, _, []) :- !.
 generate_first_row(N, GridWidth, GridHeight, SkipPos, [Pos-dole|Rest]) :-
     N > 0,
-    Pos is GridWidth - N + 1,
+    Pos is GridWidth - N,
     Pos \= SkipPos,  % Skip the position if it matches SkipPos
     N1 is N - 1,
     generate_first_row(N1, GridWidth, GridHeight, SkipPos, Rest).
 generate_first_row(N, GridWidth, GridHeight, SkipPos, Rest) :-
     N > 0,
-    Pos is GridWidth - N + 1,
+    Pos is GridWidth - N,
     Pos = SkipPos,  % If the position matches SkipPos, skip it
     N1 is N - 1,
     generate_first_row(N1, GridWidth, GridHeight, SkipPos, Rest).
@@ -101,13 +105,13 @@ generate_first_row(N, GridWidth, GridHeight, SkipPos, Rest) :-
 generate_first_column(_, _, 0, _, []) :- !.
 generate_first_column(GridWidth, GridHeight, N, SkipPos, [Pos-doprava|Rest]) :-
     N > 0,
-    Pos is (N - 1) * GridWidth + 1,
+    Pos is (N - 1) * GridWidth,
     Pos \= SkipPos,  % Skip the position if it matches SkipPos
     N1 is N - 1,
     generate_first_column(GridWidth, GridHeight, N1, SkipPos, Rest).
 generate_first_column(GridWidth, GridHeight, N, SkipPos, Rest) :-
     N > 0,
-    Pos is (N - 1) * GridWidth + 1,
+    Pos is (N - 1) * GridWidth,
     Pos = SkipPos,  % If the position matches SkipPos, skip it
     N1 is N - 1,
     generate_first_column(GridWidth, GridHeight, N1, SkipPos, Rest).
@@ -122,67 +126,126 @@ vytvor_pozicie(GridWidth, GridHeight, SkipPos, Result) :-
 
 
 % ----------------------------------------------------------------
-% Predikaty, ktore vratia okienko tajanky, kde ma zacinat
-vrat_okienko_tajanky(VyskaKrizovky, SirkaKrizovky, dole, Okienko) :-
-    Stred is div(SirkaKrizovky, 2),
-    Okienko is Stred + 1, !.
+% Predikaty, ktore vratia okienko, kde ma zacinat tajanka
+
+% vrat_okienko_tajanky(+_VyskaKrizovky, +SirkaKrizovky, +Smer, -Okienko) :-
+% Vrati okienko zaciatku tajanky na ploche cislovanej od 0 z laveho horneho rohu
+vrat_okienko_tajanky(_VyskaKrizovky, SirkaKrizovky, dole, Okienko) :-
+    Okienko is div(SirkaKrizovky, 2),
+    !.
 
 vrat_okienko_tajanky(VyskaKrizovky, SirkaKrizovky, doprava, Okienko) :-
     Riadok is div(VyskaKrizovky, 2), 
-    KoniecPredchRiadku is Riadok * SirkaKrizovky,
-    Okienko is KoniecPredchRiadku + 1, !.
+    Okienko is Riadok * SirkaKrizovky,
+    !.
 % ----------------------------------------------------------------
 
 
 
 % ----------------------------------------------------------------
-% Tento predikat vlozi tajanku na grid podla Okienka, kde zacina
+% prirad_tajanku(+Tajanka, +TajankaSmer, +Okienko, +VyskaKrizovky, +SirkaKrizovky, +VstupnaPlocha, -VystupnaPlocha) :-
+% Predikat vlozi tajanku na plochu podla Okienka, kde zacina a vrati plochu ako VystupnaPlocha
 prirad_tajanku(Tajanka, TajankaSmer, Okienko, VyskaKrizovky, SirkaKrizovky, VstupnaPlocha, VystupnaPlocha) :-
     atom_chars(Tajanka, PismenaTajanky), % Zmenim si Tajanku na list pismen
     length(PismenaTajanky, DlzkaTajanky), % Zistim velkost tajanky
+    % Test, ci je tajanka na celu sirku/dlzku krizovky
+    (TajankaSmer == doprava ->
+        DlzkaTajanky = SirkaKrizovky
+    ; 
+        DlzkaTajanky = VyskaKrizovky  
+    ),
     prirad_slovo(Tajanka, PismenaTajanky, DlzkaTajanky, tajanka, Okienko, TajankaSmer, VyskaKrizovky, SirkaKrizovky, VstupnaPlocha, VystupnaPlocha, PriradeneSlovo).
 % ----------------------------------------------------------------
 
+% vrat_stlpec_podla_okienka(+Okienko, +SirkaKrizovky, -Stlpec) :-
+% Predikat vrati cislo stlpca, kde sa nachadza Okienko
+vrat_stlpec_podla_okienka(Okienko, SirkaKrizovky, Stlpec) :- 
+    Stlpec is Okienko mod SirkaKrizovky.
+
+
+% vrat_riadok_podla_okienka(+Okienko, +SirkaKrizovky, -Riadok) :-
+% Predikat vrati cislo riadku, kde sa nachadza Okienko
+vrat_riadok_podla_okienka(Okienko, SirkaKrizovky, Riadok) :-
+    Riadok is Okienko // SirkaKrizovky.
+
 
 
 % ----------------------------------------------------------------
-% Vrati cisla policok, kde sa bude slovo nachadzat podla Okienka
-get_cisla_okienok_pre_slovo(DlzkaSlova,  Okienko, doprava, Okienka, VyskaKrizovky, SirkaKrizovky) :-
-    DlzkaSlova == SirkaKrizovky,
-    get_cisla_okienok_pre_slovo_skontrolovana_dlzka(DlzkaSlova, Okienko, doprava, Okienka, VyskaKrizovky, SirkaKrizovky),
+% vrat_cisla_okienok_pre_slovo(+DlzkaSlova, +Okienko, +Smer, -Okienka, +VyskaKrizovky, +SirkaKrizovky) :- 
+% Predikat vrati cisla okienok, kde sa bude slovo nachadzat podla zaciatocneho okienka
+% Predikat najprv skontroluje, ci sa dane slovo zmensti na plochu a ak nie, tak vrati false
+vrat_cisla_okienok_pre_slovo(DlzkaSlova,  Okienko, doprava, Okienka, VyskaKrizovky, SirkaKrizovky) :-
+    vrat_stlpec_podla_okienka(Okienko, SirkaKrizovky, StlpecZaciatku),
+    KoniecSlova is StlpecZaciatku + DlzkaSlova,
+    KoniecSlova =< SirkaKrizovky,
+    vrat_cisla_okienok_pre_slovo_skontrolovana_dlzka(DlzkaSlova, Okienko, doprava, Okienka, VyskaKrizovky, SirkaKrizovky),
     !.
 
-
-get_cisla_okienok_pre_slovo(DlzkaSlova,  Okienko, dole, Okienka, VyskaKrizovky, SirkaKrizovky) :-
-    DlzkaSlova == VyskaKrizovky,
-    get_cisla_okienok_pre_slovo_skontrolovana_dlzka(DlzkaSlova, Okienko, dole, Okienka, VyskaKrizovky, SirkaKrizovky),
+vrat_cisla_okienok_pre_slovo(DlzkaSlova,  Okienko, dole, Okienka, VyskaKrizovky, SirkaKrizovky) :-
+    vrat_riadok_podla_okienka(Okienko, SirkaKrizovky, RiadokZaciatku),
+    KoniecSlova is RiadokZaciatku + DlzkaSlova,
+    KoniecSlova =< VyskaKrizovky,
+    vrat_cisla_okienok_pre_slovo_skontrolovana_dlzka(DlzkaSlova, Okienko, dole, Okienka, VyskaKrizovky, SirkaKrizovky),
     !.
 
-get_cisla_okienok_pre_slovo_skontrolovana_dlzka(1, AktualneOkienko, _, [AktualneOkienko], VyskaKrizovky, SirkaKrizovky).
+% vrat_cisla_okienok_pre_slovo_skontrolovana_dlzka(+DlzkaSlova, +Okienko, +Smer, -Okienka, +VyskaKrizovky, +SirkaKrizovk) :- 
+% Pomocny predikat pre predikat vrat_cisla_okienok_pre_slovo(), ktory uz vracia 
+% iba listy, ak je skontrolovana dlzka slova
+vrat_cisla_okienok_pre_slovo_skontrolovana_dlzka(1, AktualneOkienko, _, [AktualneOkienko], _, _).
 
-
-get_cisla_okienok_pre_slovo_skontrolovana_dlzka(DlzkaSlova,  Okienko, doprava, [Okienko|Zvysok], VyskaKrizovky, SirkaKrizovky) :-
+vrat_cisla_okienok_pre_slovo_skontrolovana_dlzka(DlzkaSlova, Okienko, doprava, [Okienko|Zvysok], VyskaKrizovky, SirkaKrizovky) :-
     NovaDlzka is DlzkaSlova - 1,
     NoveOkianko is Okienko + 1,
-    get_cisla_okienok_pre_slovo_skontrolovana_dlzka(NovaDlzka, NoveOkianko, doprava, Zvysok, VyskaKrizovky, SirkaKrizovky).
+    vrat_cisla_okienok_pre_slovo_skontrolovana_dlzka(NovaDlzka, NoveOkianko, doprava, Zvysok, VyskaKrizovky, SirkaKrizovky).
 
-get_cisla_okienok_pre_slovo_skontrolovana_dlzka(DlzkaSlova, Okienko, dole, [Okienko|Zvysok], VyskaKrizovky, SirkaKrizovky) :-
+vrat_cisla_okienok_pre_slovo_skontrolovana_dlzka(DlzkaSlova, Okienko, dole, [Okienko|Zvysok], VyskaKrizovky, SirkaKrizovky) :-
     NovaDlzka is DlzkaSlova - 1,
     NoveOkianko is Okienko + SirkaKrizovky,
-    get_cisla_okienok_pre_slovo_skontrolovana_dlzka(NovaDlzka, NoveOkianko, dole, Zvysok, VyskaKrizovky, SirkaKrizovky).
+    vrat_cisla_okienok_pre_slovo_skontrolovana_dlzka(NovaDlzka, NoveOkianko, dole, Zvysok, VyskaKrizovky, SirkaKrizovky).
 % ----------------------------------------------------------------
 
 
 
 % ----------------------------------------------------------------
-% Predikat vytvori plochu krizovky
-prazdne_okienko(Okienko, Okienko-empty).
+% vrat_okienko_za_koncom(+DlzkaSlovam +Okienko, +Smer, +VyskaKrizovky, +SirkaKrizovky, -OkienkoZaKoncom) :-
+% Predikat vrati okienko za koncom slova zacinajucom v okienku Okienko s dlzkou DlzkaSlova
+% Ak je toto okienko mimo plochy, tak vrati false
+vrat_okienko_za_koncom(DlzkaSlova, Okienko, dole, VyskaKrizovky, SirkaKrizovky, OkienkoZaKoncom) :- 
+    vrat_riadok_podla_okienka(Okienko, SirkaKrizovky, Riadok),
+    KoniecSlova is Riadok + DlzkaSlova,
+    ( KoniecSlova < VyskaKrizovky ->
+        TrebaPridat is DlzkaSlova * SirkaKrizovky,
+        OkienkoZaKoncom is TrebaPridat + Okienko    
+    ;
+        false
+    ).
 
-vytvor_plochu(VyskaKrizovky, SirkaKrizovky, Plocha) :- %
-    PocetOkienok is VyskaKrizovky * SirkaKrizovky, %
-    numlist(1, PocetOkienok, Okienka), %
-    maplist(prazdne_okienko, Okienka, TupleOkienka), %
-    list_to_assoc(TupleOkienka, Plocha). %
+vrat_okienko_za_koncom(DlzkaSlova, Okienko, doprava, _VyskaKrizovky, SirkaKrizovky, OkienkoZaKoncom) :- 
+    vrat_stlpec_podla_okienka(Okienko, SirkaKrizovky, Stlpec),
+    KoniecSlova is Stlpec + DlzkaSlova,
+    ( KoniecSlova < SirkaKrizovky ->
+        OkienkoZaKoncom is DlzkaSlova + Okienko    
+    ;
+        false
+    ).
+% ----------------------------------------------------------------
+
+
+
+% ----------------------------------------------------------------
+% prazdne_okienko(+Okienko, -Okienko-prazdne) :- 
+% Predikat vytovri dvojicu z okienka tak, ze ku nemu prida, ze je prazdne
+prazdne_okienko(Okienko, Okienko-prazdne).
+
+% vytvor_plochu(+VyskaKrizovky, +SirkaKrizovky, -Plocha) :-
+% Predikat dostane Vysku a Sirku krizovky ako argument a vytvori plochu
+% krizovky ako asociativny list
+vytvor_plochu(VyskaKrizovky, SirkaKrizovky, Plocha) :-
+    PocetOkienok is VyskaKrizovky * SirkaKrizovky,
+    PocetOkienokMinusJedna is PocetOkienok - 1,
+    numlist(0, PocetOkienokMinusJedna, Okienka),
+    maplist(prazdne_okienko, Okienka, TupleOkienka),
+    list_to_assoc(TupleOkienka, Plocha).
 % ----------------------------------------------------------------
 
 
